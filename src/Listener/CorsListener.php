438f27e74b3a6c9bdb75aa87adb57d7b828bd427
Gosha <guerosf@yandex.ru>
2019-11-28 01:52:43 +0300

    fast resolve CORS

---
+++ src/Listener/CorsListener.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Listener;
+
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class CorsListener implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::REQUEST => ['onRequest', 9999],
+            KernelEvents::RESPONSE => ['onResponse', 9999]
+        ];
+    }
+
+    public function onRequest(RequestEvent $event): void
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        $request = $event->getRequest();
+        if ($request->getRealMethod() === 'OPTIONS') {
+            $response = new Response();
+            $event->setResponse($response);
+        }
+    }
+
+    public function onResponse(ResponseEvent $event): void
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        $response = $event->getResponse();
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT');
+        $response->headers->set('Access-Control-Allow-Headers', 'X-Header-One,X-Header-Two');
+    }
+
+}
